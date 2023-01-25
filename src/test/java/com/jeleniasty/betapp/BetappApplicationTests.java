d3c53267e17170fe69003dc04bb3efb3cc3d435c
micha <michal.jeleniasty@gmail.com>
2023-01-25 17:43:33 +0100

    Initial commit

---
+++ src/test/java/com/jeleniasty/betapp/BetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeleniasty.betapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
