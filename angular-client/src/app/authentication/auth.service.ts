ee1dbdaf01ca504d4850afe529ee0e1ec6696e51
micha <michal.jeleniasty@gmail.com>
2023-10-26 00:43:07 +0200

    Adding login component and auth service

---
+++ angular-client/src/app/authentication/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    const headers: HttpHeaders = new HttpHeaders().set(
+      'Content-Type',
+      'application/x-www-form-urlencoded'
+    );
+    const body = new URLSearchParams();
+    body.set('email', email);
+    body.set('password', password);
+
+    return this.http.post('http://localhost:8080/api/login', body.toString(), {
+      headers,
+    });
+  }
+}
---
+++ angular-client/src/app/authentication/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'betapp-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    const formValues = this.loginForm.value;
+
+    if (formValues.email && formValues.password) {
+      this.authService
+        .login(formValues.email, formValues.password)
+        .subscribe(() => {
+          this.router.navigateByUrl('/');
+        });
+    }
+  }
+}
