b76eb240fcd4e5cdc69a274ca1d244583280437b
micha <michal.jeleniasty@gmail.com>
2023-11-02 17:58:16 +0100

    Disabling header on login and register page

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  showHeader: boolean = true;
+
+  ngOnInit() {
+    this.showHeader = !(
+      window.location.href.includes('/login') ||
+      window.location.href.includes('/register')
+    );
+  }
+}
