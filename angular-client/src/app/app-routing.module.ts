66057751ddf66dfbcf88b1e3aacdc2490c76505c
micha <michal.jeleniasty@gmail.com>
2023-11-04 23:10:12 +0100

    Adding match route

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { AuthGuardService } from './authentication/auth-guard.service';
+import { MatchComponent } from './match/match.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent, canActivate: [AuthGuardService] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {
+    path: 'match/:id',
+    component: MatchComponent,
+    canActivate: [AuthGuardService],
+  },
 ];
 
 @NgModule({
