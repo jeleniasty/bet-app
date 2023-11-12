0623bdb7d5fc56f030cff31750a6e02ec28c2aaf
micha <michal.jeleniasty@gmail.com>
2023-11-12 20:53:25 +0100

    FE - changing tables content based on bet type

--- angular-client/src/app/bet/user-bets/user-bets.component.ts
+++ angular-client/src/app/bet/user-bets/user-bets.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Bet } from '../Bet';
 import { ActivatedRoute } from '@angular/router';
+import { BetType } from '../../match/BetType';
 
 @Component({
   selector: 'betapp-user-bets',
@@ -11,6 +12,8 @@ import { ActivatedRoute } from '@angular/router';
 export class UserBetsComponent implements OnInit {
   matchId: number | null = null;
   bets: Bet[] = [];
+  fullTimeResultBets: Bet[] = [];
+  correctScoreBets: Bet[] = [];
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
   ngOnInit() {
@@ -19,9 +22,15 @@ export class UserBetsComponent implements OnInit {
       this.matchId = +idString;
     }
     if (this.matchId) {
-      this.getUserBets(this.matchId).subscribe(
-        (bets: Bet[]) => (this.bets = bets)
-      );
+      this.getUserBets(this.matchId).subscribe((bets: Bet[]): void => {
+        this.fullTimeResultBets = bets.filter(
+          (bet: Bet): boolean => bet.type === BetType.FULL_TIME_RESULT
+        );
+
+        this.correctScoreBets = bets.filter(
+          (bet: Bet): boolean => bet.type === BetType.CORRECT_SCORE
+        );
+      });
     }
   }
 
