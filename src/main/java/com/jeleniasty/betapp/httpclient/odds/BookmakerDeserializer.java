2f6d795173661443430593026100df487df0248e
micha <michal.jeleniasty@gmail.com>
2023-11-27 21:02:23 +0100

    BE - added deserialization on Bookmaker class

---
+++ src/main/java/com/jeleniasty/betapp/httpclient/odds/BookmakerDeserializer.java
@@ -0,0 +1,50 @@
+package com.jeleniasty.betapp.httpclient.odds;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookmakerDeserializer extends StdDeserializer<Bookmaker> {
+
+  protected BookmakerDeserializer() {
+    this(null);
+  }
+
+  protected BookmakerDeserializer(Class vc) {
+    super(vc);
+  }
+
+  @Override
+  public Bookmaker deserialize(
+    JsonParser jsonParser,
+    DeserializationContext deserializationContext
+  ) throws IOException, JsonProcessingException {
+    JsonNode bookmaker = jsonParser.getCodec().readTree(jsonParser);
+
+    String title = bookmaker.get("title").asText();
+    Instant lastUpdate = Instant.parse(bookmaker.get("last_update").asText());
+
+    List<Outcome> outcomes = new ArrayList<>();
+    if (bookmaker.has("markets") && bookmaker.get("markets").isArray()) {
+      JsonNode marketsNode = bookmaker.get("markets").get(0);
+      if (
+        marketsNode.has("outcomes") && marketsNode.get("outcomes").isArray()
+      ) {
+        JsonNode outcomesNode = marketsNode.get("outcomes");
+        for (JsonNode outcomeNode : outcomesNode) {
+          String outcomeName = outcomeNode.get("name").asText();
+          double outcomePrice = outcomeNode.get("price").asDouble();
+          outcomes.add(new Outcome(outcomeName, outcomePrice));
+        }
+      }
+    }
+
+    return new Bookmaker(title, lastUpdate, outcomes);
+  }
+}
