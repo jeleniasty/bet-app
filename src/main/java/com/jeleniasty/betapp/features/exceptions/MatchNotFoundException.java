f9eb65211ed8d82e1a220900b7f01c7da080669f
micha <michal.jeleniasty@gmail.com>
2023-11-30 21:37:34 +0100

    BE - added custom exception

--- src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
+++ src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
@@ -1,8 +1,26 @@
 package com.jeleniasty.betapp.features.exceptions;
 
+import java.time.LocalDateTime;
+
 public class MatchNotFoundException extends RuntimeException {
 
   public MatchNotFoundException(Long matchId) {
     super("Match with id '" + matchId + "' not found");
+  }
+
+  public MatchNotFoundException(
+    String homeTeamName,
+    String awayTeamName,
+    LocalDateTime dateTime
+  ) {
+    super(
+      "Match between " +
+      homeTeamName +
+      " and " +
+      awayTeamName +
+      " of " +
+      dateTime +
+      " not found."
+    );
   }
 }
