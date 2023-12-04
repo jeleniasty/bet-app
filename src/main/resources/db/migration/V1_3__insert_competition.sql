1894e7a36e0a166f5df71f8b870efc29f233f8d6
micha <michal.jeleniasty@gmail.com>
2023-12-04 20:15:36 +0100

    BE - removing test sql scripts

--- src/main/resources/db/migration/V1_3__insert_competition.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO betapp.competition (name, code, type, season, emblem, start_date, end_date)
-VALUES ('FIFA World Cup', 'WC', 'CUP', 2022,'flaga ziu ziu .jpg', '2023-09-23', '2024-06-01');
