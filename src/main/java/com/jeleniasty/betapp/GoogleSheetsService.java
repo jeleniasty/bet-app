2e600dd8b841e72ba499f1d14847a97a7e3c7d41
micha <michal.jeleniasty@gmail.com>
2023-12-06 20:44:15 +0100

    BE - added google sheets api

---
+++ src/main/java/com/jeleniasty/betapp/GoogleSheetsService.java
@@ -0,0 +1,33 @@
+package com.jeleniasty.betapp;
+
+import com.jeleniasty.betapp.config.GoogleAuthorizationConfig;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GoogleSheetsService {
+
+  @Value("${googlesheets.spreadsheet-id}")
+  private String spreadsheetId;
+
+  private final GoogleAuthorizationConfig googleAuthorizationConfig;
+
+  public List<List<Object>> getSpreadsheetValues()
+    throws IOException, GeneralSecurityException {
+    return googleAuthorizationConfig
+      .getSheetsService()
+      .spreadsheets()
+      .values()
+      .get(spreadsheetId, "Arkusz1")
+      .execute()
+      .getValues();
+  }
+}
+//TODO GoogleSheetsService more reusable
