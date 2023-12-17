9dd24b14c30fc10ecd338d9dea9a280421ca7a1f
micha <michal.jeleniasty@gmail.com>
2023-12-17 23:43:52 +0100

    BE - method name refactor

--- src/main/java/com/jeleniasty/betapp/features/team/TeamRepository.java
+++ src/main/java/com/jeleniasty/betapp/features/team/TeamRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
-  Optional<Team> findByNameAndCode(String name, String code);
+  Optional<Team> findByName(String name);
 }
