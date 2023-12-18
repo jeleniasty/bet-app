25e3fef776e0c4f06c51a9537bbc18f91df63a60
micha <michal.jeleniasty@gmail.com>
2023-12-18 20:40:20 +0100

    BE - scheduler service refactor

--- src/main/java/com/jeleniasty/betapp/features/scheduler/SchedulerService.java
+++ src/main/java/com/jeleniasty/betapp/features/scheduler/SchedulerService.java
@@ -1,8 +1,5 @@
 package com.jeleniasty.betapp.features.scheduler;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ScheduledFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.TaskScheduler;
@@ -15,32 +12,8 @@ public class SchedulerService {
 
   private final TaskScheduler taskScheduler;
 
-  private final Map<Long, ScheduledFuture<?>> scheduledTasksMap =
-    new HashMap<>();
-
-  public void scheduleTask(Task task) {
-    var scheduledTask =
-      this.taskScheduler.scheduleAtFixedRate(
-          task.task(),
-          task.delay(),
-          task.period()
-        );
-
-    this.scheduledTasksMap.put(task.name(), scheduledTask);
-    log.info(
-      "Task with id '" +
-      task.name() +
-      "' scheduled. Starting at " +
-      task.delay()
-    );
-  }
-
-  public void cancelScheduledTask(Long taskId) {
-    var scheduledTask = this.scheduledTasksMap.get(taskId);
-    if (scheduledTask != null) {
-      scheduledTask.cancel(true);
-      this.scheduledTasksMap.remove(taskId);
-      log.info("Task with id '" + taskId + "' removed.");
-    }
+  public void scheduleTaskExecution(DelayedTask task) {
+    log.info("Scheduling task with id: " + task.id());
+    this.taskScheduler.schedule(task.task(), task.delay());
   }
 }
