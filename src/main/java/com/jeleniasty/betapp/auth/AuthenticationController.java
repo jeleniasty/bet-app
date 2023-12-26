841a941e85b088cddd90dfd06f148ae3d20c8f92
micha <michal.jeleniasty@gmail.com>
2023-12-26 20:07:30 +0100

    BE - AuthenticationController refactor

--- src/main/java/com/jeleniasty/betapp/auth/AuthenticationController.java
+++ src/main/java/com/jeleniasty/betapp/auth/AuthenticationController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,20 +15,17 @@ public class AuthenticationController {
 
   private final AuthenticationService authService;
 
-  @PostMapping(
-    value = "/api/register",
-    consumes = "application/x-www-form-urlencoded"
-  )
-  public ResponseEntity<Void> register(RegisterRequest request) {
-    authService.register(request);
-    return ResponseEntity.status(HttpStatus.CREATED).build();
+  @PostMapping("/api/register")
+  public ResponseEntity<AuthResponse> register(
+    @RequestBody RegisterRequest request
+  ) {
+    return ResponseEntity
+      .status(HttpStatus.CREATED)
+      .body(authService.register(request));
   }
 
-  @PostMapping(
-    value = "/api/login",
-    consumes = "application/x-www-form-urlencoded"
-  )
-  public ResponseEntity<AuthResponse> login(AuthRequest request) {
+  @PostMapping("/api/login")
+  public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
     return ResponseEntity.ok(authService.authenticate(request));
   }
 }
