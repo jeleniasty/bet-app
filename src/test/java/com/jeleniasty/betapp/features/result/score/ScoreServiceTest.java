a823a5dc3343ce2b0887ea60be8b7c31a97e2e79
micha <michal.jeleniasty@gmail.com>
2023-12-28 19:11:22 +0100

    BE - added ScoreServiceTest

--- src/test/java/com/jeleniasty/betapp/features/result/score/ScoreServiceTest.java
+++ src/test/java/com/jeleniasty/betapp/features/result/score/ScoreServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,20 +28,38 @@ class ScoreServiceTest {
   );
 
   @Test
-  void saveScore() {
-    assertThat(postgreSQLContainer.isCreated()).isTrue();
-    assertThat(postgreSQLContainer.isRunning()).isTrue();
-
+  @Transactional
+  void getScore_should_save_new_score_in_db_if_not_exists() {
     //arrange
     var scoreDTO = new ScoreDTO(1, 1);
 
     //act
-    scoreService.fetchOrSave(scoreDTO);
+    scoreService.getScore(scoreDTO);
 
     //assert
     var actualScore = scoreRepository.findByHomeAndAway(1, 1);
+    var savedScoresCount = scoreRepository.findAll().size();
 
+    assertThat(savedScoresCount).isEqualTo(1);
     assertThat(actualScore.get().getHome()).isEqualTo(scoreDTO.home());
     assertThat(actualScore.get().getAway()).isEqualTo(scoreDTO.away());
+  }
+
+  @Test
+  @Transactional
+  void getScore_should_get_score_from_db_if_exists() {
+    //arrange
+    var scoreDTO = new ScoreDTO(1, 1);
+
+    //act
+    scoreRepository.save(new Score(scoreDTO.home(), scoreDTO.away()));
+    var actualScore = scoreService.getScore(scoreDTO);
+
+    //assert
+    var savedScoresCount = scoreRepository.findAll().size();
+
+    assertThat(savedScoresCount).isEqualTo(1);
+    assertThat(actualScore.getHome()).isEqualTo(scoreDTO.home());
+    assertThat(actualScore.getAway()).isEqualTo(scoreDTO.away());
   }
 }
