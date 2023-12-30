328f24918fe95efca0488d1566eaad20a76584ed
micha <michal.jeleniasty@gmail.com>
2023-12-30 16:14:53 +0100

    BE - removed unused constructor

--- src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
+++ src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
@@ -1,26 +1,8 @@
 package com.jeleniasty.betapp.features.exceptions;
 
-import java.time.LocalDateTime;
-
 public class MatchNotFoundException extends RuntimeException {
 
   public MatchNotFoundException(Long matchId) {
     super("Match with id '" + matchId + "' not found");
-  }
-
-  public MatchNotFoundException(
-    String homeTeamName,
-    String awayTeamName,
-    LocalDateTime dateTime
-  ) {
-    super(
-      "Match between " +
-      homeTeamName +
-      " and " +
-      awayTeamName +
-      " of " +
-      dateTime +
-      " not found."
-    );
   }
 }
