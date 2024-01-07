6fea301466c6a668092821e90f04bc9fbfadafe6
micha <michal.jeleniasty@gmail.com>
2024-01-07 21:46:33 +0100

    BE - created static method factory

--- src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
+++ src/main/java/com/jeleniasty/betapp/features/exceptions/MatchNotFoundException.java
@@ -2,7 +2,19 @@ package com.jeleniasty.betapp.features.exceptions;
 
 public class MatchNotFoundException extends RuntimeException {
 
-  public MatchNotFoundException(Long matchId) {
-    super("Match with id '" + matchId + "' not found");
+  private MatchNotFoundException(String message) {
+    super(message);
+  }
+
+  public static MatchNotFoundException withId(Long matchId) {
+    return new MatchNotFoundException(
+      "Match with id '" + matchId + "' not found"
+    );
+  }
+
+  public static MatchNotFoundException withExternalId(Long externalId) {
+    return new MatchNotFoundException(
+      "Match with external id '" + externalId + "' not found"
+    );
   }
 }
