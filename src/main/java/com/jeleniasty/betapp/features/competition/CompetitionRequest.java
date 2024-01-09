e93cc615805d3fdf82a02aeaef6523d4bb90a08f
micha <michal.jeleniasty@gmail.com>
2024-01-09 16:10:06 +0100

    BE - renamed value object

---
+++ src/main/java/com/jeleniasty/betapp/features/competition/CompetitionRequest.java
@@ -0,0 +1,3 @@
+package com.jeleniasty.betapp.features.competition;
+
+public record CompetitionRequest(String code, int season) {}
--- src/main/java/com/jeleniasty/betapp/features/competition/CreateCompetitonRequest.java
+++
@@ -1,3 +0,0 @@
-package com.jeleniasty.betapp.features.competition;
-
-public record CreateCompetitonRequest(String code, int season) {}
