29046b77f3fd8a8248e9260f74032201330576f7
micha <michal.jeleniasty@gmail.com>
2024-01-09 16:10:23 +0100

    BE - added new method to CompetitionRepository

--- src/main/java/com/jeleniasty/betapp/features/competition/CompetitionRepository.java
+++ src/main/java/com/jeleniasty/betapp/features/competition/CompetitionRepository.java
@@ -1,5 +1,7 @@
 package com.jeleniasty.betapp.features.competition;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +9,6 @@ public interface CompetitionRepository
   extends JpaRepository<Competition, Long> {
   boolean existsByCodeAndSeason(String code, int season);
   Optional<Competition> findCompetitionByCodeAndSeason(String code, int season);
+
+  List<Competition> findCompetitionByEndDateAfter(LocalDate date);
 }
