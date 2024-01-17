bc7eb87b550478e8148020949eb91c615d3d3872
micha <michal.jeleniasty@gmail.com>
2024-01-17 20:58:54 +0100

    BE - added authorization to dictionary sync endpoint

--- src/main/java/com/jeleniasty/betapp/features/team/TeamController.java
+++ src/main/java/com/jeleniasty/betapp/features/team/TeamController.java
@@ -1,6 +1,7 @@
 package com.jeleniasty.betapp.features.team;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,8 @@ public class TeamController {
   private final TeamService teamService;
 
   @PostMapping("team/dictionary")
+  @PreAuthorize(value = "hasAuthority('ADMIN')")
   public void synchroniseDictionary() {
     this.teamService.synchroniseDictionary();
   }
-  //TODO add authorization
-
 }
