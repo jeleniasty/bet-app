d461f2c2b4e52ef5e2ac05a549a0ffa7815cde42
micha <michal.jeleniasty@gmail.com>
2024-01-29 10:23:46 +0100

    BE - changed GCP authorization

--- src/main/java/com/jeleniasty/betapp/config/GoogleAuthorizationConfig.java
+++ src/main/java/com/jeleniasty/betapp/config/GoogleAuthorizationConfig.java
@@ -1,6 +1,7 @@
 package com.jeleniasty.betapp.config;
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.sheets.v4.Sheets;
@@ -23,14 +24,17 @@ public class GoogleAuthorizationConfig {
   @Value("${googlesheets.apiKey}")
   private String apiKey;
 
-  public GoogleAuthorizationConfig() {}
-
   public Sheets getSheetsService()
     throws GeneralSecurityException, IOException {
+    // Create a simple HttpRequestInitializer (you can customize this based on your needs)
+    HttpRequestInitializer requestInitializer = httpRequest -> {
+      // No additional initialization for now
+    };
+
     return new Sheets.Builder(
       GoogleNetHttpTransport.newTrustedTransport(),
       JSON_FACTORY,
-      null
+      requestInitializer
     )
       .setApplicationName(applicationName)
       .build();
