cfe02b89013ac2c30e35998b36d96970e6534ac9
micha <michal.jeleniasty@gmail.com>
2024-01-29 10:11:51 +0100

    BE - changed GCP authorization

--- src/main/java/com/jeleniasty/betapp/config/GoogleAuthorizationConfig.java
+++ src/main/java/com/jeleniasty/betapp/config/GoogleAuthorizationConfig.java
@@ -1,23 +1,12 @@
 package com.jeleniasty.betapp.config;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
-import com.google.api.services.sheets.v4.SheetsScopes;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,58 +19,18 @@ public class GoogleAuthorizationConfig {
   @Value("${googlesheets.name}")
   private String applicationName;
 
-  @Value("${googlesheets.credentials-filepath}")
-  private String credentialsFilePath;
-
-  @Value("${googlesheets.tokens-directorypath}")
-  private String tokensDirectoryPath;
+  @Getter
+  @Value("${googlesheets.apiKey}")
+  private String apiKey;
 
   public GoogleAuthorizationConfig() {}
 
-  private Credential authorize() throws IOException, GeneralSecurityException {
-    InputStream in =
-      GoogleAuthorizationConfig.class.getResourceAsStream(credentialsFilePath);
-
-    GoogleClientSecrets clientSecrets = null;
-    if (in != null) {
-      clientSecrets =
-        GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-    }
-
-    List<String> scopes = Collections.singletonList(
-      SheetsScopes.SPREADSHEETS_READONLY
-    );
-
-    GoogleAuthorizationCodeFlow flow = null;
-    if (clientSecrets != null) {
-      flow =
-        new GoogleAuthorizationCodeFlow.Builder(
-          GoogleNetHttpTransport.newTrustedTransport(),
-          JSON_FACTORY,
-          clientSecrets,
-          scopes
-        )
-          .setDataStoreFactory(
-            new FileDataStoreFactory(new File(tokensDirectoryPath))
-          )
-          .setAccessType("offline")
-          .build();
-    }
-
-    LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder()
-      .setPort(8888)
-      .build();
-    return new AuthorizationCodeInstalledApp(flow, localServerReceiver)
-      .authorize("user");
-  }
-
   public Sheets getSheetsService()
     throws GeneralSecurityException, IOException {
-    Credential credential = authorize();
     return new Sheets.Builder(
       GoogleNetHttpTransport.newTrustedTransport(),
       JSON_FACTORY,
-      credential
+      null
     )
       .setApplicationName(applicationName)
       .build();
--- src/main/java/com/jeleniasty/betapp/utils/GoogleSheetsService.java
+++ src/main/java/com/jeleniasty/betapp/utils/GoogleSheetsService.java
@@ -26,6 +26,7 @@ public class GoogleSheetsService {
       .spreadsheets()
       .values()
       .get(spreadsheetId, "Arkusz1")
+      .setKey(googleAuthorizationConfig.getApiKey())
       .execute()
       .getValues();
   }
